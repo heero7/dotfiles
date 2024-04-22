228f9537b459ef6be733d22fe265ae0af5076027
heero7 <kevin7.mudia@gmail.com>
2024-04-21 22:46:31 -0500

    feat(lsp): tsserver

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -21,9 +21,8 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-
-      })
+      lspconfig.lua_ls.setup({})
+      lspconfig.tsserver.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
