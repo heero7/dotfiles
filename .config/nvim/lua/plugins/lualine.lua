1be7032ab8f6335322a4cbab5aebf9a125f0b6fd
heero7 <kevin7.mudia@gmail.com>
2024-04-21 23:12:03 -0500

    feat: linters and formatters (also formatted lua files)

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,5 @@
 return {
-  'nvim-lualine/lualine.nvim',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = { theme = 'tokyonight' }
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = { theme = "tokyonight" },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,18 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "javascript",
-        "html",
-        "vimdoc"
-      },
-      sync_install = false,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"javascript",
+				"html",
+				"vimdoc",
+			},
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
