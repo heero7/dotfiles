1d17d6be9dd7527857a761cae0b783491e297c03
heero7 <kevin7.mudia@gmail.com>
2024-04-22 10:33:22 -0500

    chore: update options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,7 +6,10 @@ opt.softtabstop = 2
 opt.shiftwidth = 2
 opt.number = true
 opt.relativenumber = true
+opt.autoindent = true
 
 opt.wrap = false
 
 opt.termguicolors = true
+
+opt.signcolumn = "yes"
