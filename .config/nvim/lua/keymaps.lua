61890b1673befface9983729de95717e0f0bbdd7
heero7 <kevin7.mudia@gmail.com>
2025-03-29 19:32:44 -0500

    starting over neovim configuration

--- .config/nvim/lua/keymaps.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-
-local keymap = vim.keymap
-
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
